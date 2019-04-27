04884530413d5e38debc7cdcb99c4395dcb9c4cd
Naoko Kai <okashimochi@gmail.com>
2019-04-28 01:49:25 +0900

    made stoker

--- cheese-academy/js/main.js
+++ cheese-academy/js/main.js
@@ -4,6 +4,30 @@
   let screenWidth = window.screen.width;
   const darkB = document.getElementById('d-btn');
   const lightB = document.getElementById('l-btn');
+  const mask = document.getElementById('mask');
+  const submitButton = document.getElementById('submit-button'); 
+  const modalClose = document.getElementById('modal__close'); 
+  const modal = document.getElementById('modal'); 
+  const stoker = document.getElementById('stoker');
+
+  let stokerX = 0;
+  let stokerY = 0;
+  let currentX = 0;
+  let currentY = 0;
+
+  document.body.addEventListener('mousemove', (e) => {
+    stokerX = e.clientX;
+    stokerY = e.clientY; 
+  });
+
+  tick();
+
+  function tick() {
+    requestAnimationFrame(tick);
+    currentX += (stokerX - currentX) * 0.01;
+    currentY += (stokerY - currentY) * 0.01;
+    stoker.style.transform = `translate(${currentX}px, ${currentY}px)`;
+  }
 
   darkB.addEventListener('click', ()=> {
     darkB.classList.remove("open");
@@ -15,7 +39,7 @@
     lightB.classList.add("open");
   });
 
-  lightB.addEventListener('click', (e)=> {
+  lightB.addEventListener('click', ()=> {
     darkB.classList.remove("open");
     lightB.classList.remove("open");
 
@@ -31,12 +55,18 @@
   });
 
   if (screenWidth > 800) {
-    console.log(screenWidth);
     navScrollVersion.classList.remove('open');
     navScrollVersion.classList.add('hidden');
   }
 
-  
+  submitButton.addEventListener('click', () => {
+    mask.classList.remove('modal--hidden');
+    modal.classList.remove('modal--hidden');
 
+    modalClose.addEventListener('click', () => {
+      mask.classList.add('modal--hidden');
+      modal.classList.add('modal--hidden');
+    })
+  })
 
 };
